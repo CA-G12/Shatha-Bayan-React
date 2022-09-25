94886ca82a52a9079818799799801b5d5c4eb938
shathakh <shathaqudaih17@gmail.com>
2022-09-25 12:37:00 +0300

    add edit

--- my-app/src/components/Main/Cards.js
+++ my-app/src/components/Main/Cards.js
@@ -1,91 +1,72 @@
-import React from 'react'
-import { getMovieData } from '../../utils/getMovieData';
-import { Card } from './card';
-
+import React from "react";
+import { getMovieData } from "../../utils/getMovieData";
 
 export class Cards extends React.Component {
-
     constructor(props) {
         super();
     }
-
-    state = {
-        loading: false,
-    }
-
-    URL = 'https://yts.mx/api/v2/list_movies.json?limit=32&page=2'
+    // state = {
+    //     favorite: []
+    // }
+    URL = "https://yts.mx/api/v2/list_movies.json?limit=32&page=2";
     componentDidMount() {
-        this.setState({ loading: true })
         getMovieData(this.URL)
-            .then(data => {
-                return data.data.movies
-            })
-            .then(data => {
-                this.props.setData({ data })
-            })
-
-        this.setState({ loading: false })
+            .then((data) => data.data.movies)
+            .then((data) => this.props.setData({ data }));
     }
 
-    isFav = (item) => {
-        const { favorite } = this.props
-        return Boolean(favorite.find(i => i.id === item.id))
-    }
-
-    toggleFav = (ele, color) => {
-        const isFavItem = this.isFav(ele)
-        let color2
-        if (isFavItem) {
-            this.props.removeFav(ele)
-            color2 = 'gray'
-            color(color2)
-        } else {
-            this.props.addFav(ele)
-            color2 = 'red'
-            color(color2)
+    componentDidUpdate(prevProps) {
+        console.log("prev", prevProps.favorite);
+        console.log('state', this.props.favorite);
+        if (prevProps.favorite !== this.props.favorite) {
+            localStorage.setItem("favorite", JSON.stringify(this.props.favorite));
         }
-        // isFavItem ? this.props.removeFav(ele) : this.props.addFav(ele)
     }
-
     render() {
+        console.log("card data", this.props.data);
         const cardsData = this.props.data;
-
-        if (this.props.error) return <h1 className='error'>{this.props.error}</h1>
-        if (!cardsData || this.state.loading) return <div className='loading'>Loading...</div>;
-        return <div className='container'>
-            {cardsData.data.map(ele =>
-                <Card key={ele.id}ele={ele} toggleFav={this.toggleFav} isFav={this.isFav}></Card>
-            )}
-        </div>
+        if (!cardsData) return <div>Loading...</div>;
+        console.log(this.props.favorite);
+        return (
+            <>
+                {cardsData.data.map((ele) => (
+                    <>
+                        <p>{ele.title}</p>
+                        <button
+                            onClick={() => {
+                                const { favorite } = this.props;
+                                if (favorite.indexOf(ele) === -1) {
+                                    this.props.setFavorite([...favorite, ele]);
+                                } else {
+                                    const newList = favorite.filter(x => x.id !== ele.id)
+                                    this.props.setFavorite(newList);
+                                }
+                            }}
+                        >
+                            Click
+                        </button>
+                    </>
+                ))}
+            </>
+        );
     }
-
 }
 
 export class List extends React.Component {
     constructor(props) {
         super();
     }
-    // state={
-    //     close: {display:'none'}
-    // }
-
     render() {
-        const movies = JSON.parse(localStorage.getItem('favorite')) || []
-        if (movies.length === 0) return <div className='popup-container' style={this.props.close}>
-            <div className='popup'>
-                <h1>No Favorite Movies</h1>
-            </div>
-        </div>
-        return <div className='popup-container' style={this.props.close}>
-            <div className='popup'>
-                {/* <i className="fa-solid fa-xmark" onClick={()=> this.props.handelClose("hidden")}></i> */}
-                {movies.map(ele => <div className='card'>
-                    <img src={ele.medium_cover_image} alt="card" />
-                    <div className='details'>
-                        <p >{ele.title}</p>
-                    </div>
-                </div>)}
-            </div>
-        </div>
+        console.log("list", this.props.favorites);
+        const movies = JSON.parse(localStorage.getItem("favorite"));
+        <i class="fa-solid fa-xmark"></i>
+        if (movies.length === 0) return <p>no movies</p>;
+        return (
+            <>
+                {movies.map((ele) => (
+                    <li>{ele.id}</li>
+                ))}
+            </>
+        );
     }
 }
